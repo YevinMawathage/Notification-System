7dc2fef2313ff0f608f59bdf7ad8ccb2f926206e
Yevin Mawathage <mynsmedonza@students.nsbm.ac.lk>
2026-02-28 11:19:37 +0530

    database connection

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -18,11 +21,44 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Entry Point"))
 	log.Println("testing")
 }
+
+var db *sql.DB
+
+func connectDB() {
+	psqInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	var err error
+	db, err := sql.Open("postgres", psqInfo)
+	if err != nil {
+		log.Fatal("Database is unreachable: ", err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected to the PostgreSQL database!")
+}
+
+//main Function
+
 func main() {
-	err := godotenv.Load()
+
+	err := godotenv.Load(".env")
 	if err != nil {
 		log.Println("Error loading .env file")
 	}
+
+	//Database Connection
+	connectDB()
 
 	port := os.Getenv("PORT")
 	if port == "" {
